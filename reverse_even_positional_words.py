51a86e785e8c9faacd9ff76f83604222e62bd2f2
nagasowmyakovvuri <97426041+nagasowmyakovvuri@users.noreply.github.com>
2022-07-06 15:29:40 +0530

    n=input() l=n.split(' ') for i in range(len(l)):     if i%2==0:         j=l[i]         print(j[::-1],end=' ')     else:         print(l[i],end=' ')

--- reverse_even_positional_words.py
+++ reverse_even_positional_words.py
@@ -2,7 +2,9 @@ n=input()
 l=n.split(' ')
 for i in range(len(l)):
     if i%2==0:
-        k=l[i]
-        print(k[::-1],end=' ')
+        j=l[i]
+        print(j[::-1],end=' ')
     else:
         print(l[i],end=' ')
+
+    
