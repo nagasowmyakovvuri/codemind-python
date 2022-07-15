1ac7b07d5a3cfcc1b3400d75b1151df8f06c4d1a
nagasowmyakovvuri <97426041+nagasowmyakovvuri@users.noreply.github.com>
2022-07-15 14:22:14 +0530

    n=input() k=n.split(' ') s='aeiou' p=[] for i in k:     i=i.lower()     d=0     for j in i:         if j in s:             d+=1     p.append(d) l=0 for i in k:     i=i.lower()     d=0     for j in i:         if j in s:             d+=1     if d==max(p):         l+=1 print(l)

---
+++ count_words_with_maximum_vowels.py
@@ -0,0 +1,21 @@
+n=input()
+k=n.split(' ')
+s='aeiou'
+p=[]
+for i in k:
+    i=i.lower()
+    d=0
+    for j in i:
+        if j in s:
+            d+=1
+    p.append(d)
+l=0
+for i in k:
+    i=i.lower()
+    d=0
+    for j in i:
+        if j in s:
+            d+=1
+    if d==max(p):
+        l+=1
+print(l)
