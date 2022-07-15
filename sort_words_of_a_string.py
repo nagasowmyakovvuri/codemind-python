c4c41f63185c748ce817b2da73645a1dc4b09464
nagasowmyakovvuri <97426041+nagasowmyakovvuri@users.noreply.github.com>
2022-07-15 19:59:30 +0530

    n=input() l=n.split(' ') for i in l:     a=[]     s=[]     d=[]     u=0     h=0     o=[]     f=''     for j in range(len(i)):         if i[j] in '!@#$%^&*(){}[]':             s.append(j)             d.append(i[j])         else:             o.append(i[j])     o.sort()     for x in range(len(i)):         if x in s:             a.append(d[u])             u+=1         else:             a.append(o[h])             h+=1     for i in a:         f+=i     print(f,end=' ')

---
+++ sort_words_of_a_string.py
@@ -0,0 +1,28 @@
+n=input()
+l=n.split(' ')
+for i in l:
+    a=[]
+    s=[]
+    d=[]
+    u=0
+    h=0
+    o=[]
+    f=''
+    for j in range(len(i)):
+        if i[j] in '!@#$%^&*(){}[]':
+            s.append(j)
+            d.append(i[j])
+        else:
+            o.append(i[j])
+    o.sort()
+    for x in range(len(i)):
+        if x in s:
+            a.append(d[u])
+            u+=1
+        else:
+            a.append(o[h])
+            h+=1
+    for i in a:
+        f+=i
+    print(f,end=' ')
+        
