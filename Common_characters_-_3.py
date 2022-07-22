b5e08b3bf5ee0b38e1c45f024b9166f09170fe52
nagasowmyakovvuri <97426041+nagasowmyakovvuri@users.noreply.github.com>
2022-07-22 15:48:49 +0530

    n=input().lower( ) l=n.split(' ') s='' for i in l[0]:     k=0     for j in range(1,len(l)):         if i in l[j]:             k+=1     if k==len(l)-1:         s+=i if len(s)!=0:     print(min(s)) else:     print('-1')

---
+++ Common_characters_-_3.py
@@ -0,0 +1,14 @@
+n=input().lower( )
+l=n.split(' ')
+s=''
+for i in l[0]:
+    k=0
+    for j in range(1,len(l)):
+        if i in l[j]:
+            k+=1
+    if k==len(l)-1:
+        s+=i
+if len(s)!=0:
+    print(min(s))
+else:
+    print('-1')
