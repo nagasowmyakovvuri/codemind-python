2fa420ae93bc01b18305716423032088f71d4db5
nagasowmyakovvuri <97426041+nagasowmyakovvuri@users.noreply.github.com>
2022-07-22 14:49:05 +0530

    n=int(input()) l=list(map(int,input().split())) k=[] m=[] for i in l:     if i%2!=0:         k.append(i)     else:         m.append(i)
    o=[]
    i=0
    if len(k)>len(m):
        for i in range(len(m)):
            o.append(k[i])
            o.append(m[i])
        for i in range(len(m),len(k)):
            o.append(k[i])
            
        
    elif len(k)<len(m):
        i=0
        for i in range(len(k)):
            o.append(k[i])
            o.append(m[i])
        for i in range(len(k),len(m)):
            o.append(m[i])
            
        
        
    else:
        i=0
        for i in range(len(m)):
            o.append(k[i])
            o.append(m[i])
    if len(o)%2!=0:
        o.append(0)
    print(*o)

---
+++ odd_even_pairs.py
@@ -0,0 +1,41 @@
+n=int(input())
+l=list(map(int,input().split()))
+k=[]
+m=[]
+for i in l:
+    if i%2!=0:
+        k.append(i)
+    else:
+        m.append(i)
+
+o=[]
+i=0
+if len(k)>len(m):
+    for i in range(len(m)):
+        o.append(k[i])
+        o.append(m[i])
+    for i in range(len(m),len(k)):
+        o.append(k[i])
+        
+    
+elif len(k)<len(m):
+    i=0
+    for i in range(len(k)):
+        o.append(k[i])
+        o.append(m[i])
+    for i in range(len(k),len(m)):
+        o.append(m[i])
+        
+    
+    
+else:
+    i=0
+    for i in range(len(m)):
+        o.append(k[i])
+        o.append(m[i])
+if len(o)%2!=0:
+    o.append(0)
+print(*o)
+    
+    
+        
