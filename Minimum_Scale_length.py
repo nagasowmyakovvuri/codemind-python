21a67723b2a2a20a889749b6a829052d9cf2df36
nagasowmyakovvuri <97426041+nagasowmyakovvuri@users.noreply.github.com>
2022-10-02 19:37:33 +0530

    n=int(input()) l=list(map(int,input().split())) k=min(l) for i in range(1,k+1):     h=0     for j in l:         if j%i==0:             h+=1     if h==len(l):        hcf=i print(hcf)

---
+++ Minimum_Scale_length.py
@@ -0,0 +1,11 @@
+n=int(input())
+l=list(map(int,input().split()))
+k=min(l)
+for i in range(1,k+1):
+    h=0
+    for j in l:
+        if j%i==0:
+            h+=1
+    if h==len(l):
+       hcf=i
+print(hcf)
