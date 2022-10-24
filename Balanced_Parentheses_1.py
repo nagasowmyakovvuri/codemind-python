3d2f51784eeee3435f8d9490299b54c398d5c888
nagasowmyakovvuri <97426041+nagasowmyakovvuri@users.noreply.github.com>
2022-10-24 12:31:09 +0530

    def paraCheck( seq ):    while True:        if '()' in seq :            seq = seq.replace ( '()' , '' )        elif '{}' in seq :            seq = seq.replace ( '{}' , '' )        elif '[]' in seq :            seq = seq.replace ( '[]' , '' )        else :            return not seq
    seq = input()
    print(paraCheck(seq))

---
+++ Balanced_Parentheses_1.py
@@ -0,0 +1,13 @@
+def paraCheck( seq ):  
+   while True:  
+       if '()' in seq :  
+           seq = seq.replace ( '()' , '' )  
+       elif '{}' in seq :  
+           seq = seq.replace ( '{}' , '' )  
+       elif '[]' in seq :  
+           seq = seq.replace ( '[]' , '' )  
+       else :  
+           return not seq  
+  
+seq = input()
+print(paraCheck(seq))
