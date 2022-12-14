361956d447c1ba61e294cc5b2751fe644a8fd511
nagasowmyakovvuri <97426041+nagasowmyakovvuri@users.noreply.github.com>
2022-12-14 11:11:36 +0530

    n=input() k=n.split() l=list(sorted(k,key=len)) for i in range(0,len(l)-1):     if len(l[i])==len(l[i+1]) and l[i]>l[i+1]:         l[i],l[i+1]=l[i+1],l[i] print(" ".join(l))

--- sort_the_words_based_on_length.py
+++ sort_the_words_based_on_length.py
@@ -1,13 +1,7 @@
 n=input()
 k=n.split()
 l=list(sorted(k,key=len))
-
-#print(l)
 for i in range(0,len(l)-1):
-    if l[i]>l[i+1] and len(l[i])==len(l[i+1]):
-        p=l[i]
-        l[i]=l[i+1]
-        l[i+1]=p
-    else:
-        i+=1
+    if len(l[i])==len(l[i+1]) and l[i]>l[i+1]:
+        l[i],l[i+1]=l[i+1],l[i]
 print(" ".join(l))
